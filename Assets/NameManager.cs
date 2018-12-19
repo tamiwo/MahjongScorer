bddb05157635a714e2b245e75ce78d2aa643d17b
tamiwo <tamiwo.otutama@gmail.com>
2018-12-20 00:31:45 +0900

    update:名前設定

---
+++ Assets/NameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NameManager : MonoBehaviour {
+
+    public string[] NameList;
+    public GameObject[] players;
+
+    PlayerManager[] _playerMng;
+
+    // Use this for initialization
+    void Start () {
+        _playerMng = new PlayerManager[players.Length];
+        var i = 0;
+        foreach (GameObject p in players) {
+            _playerMng[i] = p.GetComponent<PlayerManager>();
+            _playerMng[i].SetName(NameList[i]);
+            i += 1;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/PlayerManager.cs
+++ Assets/PlayerManager.cs
@@ -6,18 +6,24 @@ using UnityEngine.UI;
 public class PlayerManager : MonoBehaviour {
 
     private Text _score;
+    private Text _name;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Awake () {
         _score = transform.Find("ScorePanel").Find("Score").gameObject.GetComponent<Text>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        _name = transform.Find("NameButton").Find("Name").gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
     public void SetScore( int score ) {
         _score.text = score.ToString();
+    }
+
+    public void SetName( string name ) {
+        _name.text = name;
     }
 }
