27dbfccbf6f924734e784d94f9d2d73e832cd509
tamiwo <tamiwo.otutama@gmail.com>
2018-12-20 00:12:18 +0900

    update:初期スコア

---
+++ Assets/PlayerManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour {
+
+    private Text _score;
+
+	// Use this for initialization
+	void Start () {
+        _score = transform.Find("ScorePanel").Find("Score").gameObject.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetScore( int score ) {
+        _score.text = score.ToString();
+    }
+}
---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+    public GameObject[] players;
+    public int startScore;
+
+    PlayerManager[] _playerMng;
+
+    void Start()
+    {
+        _playerMng = new PlayerManager[players.Length];
+        var i = 0;
+        foreach (GameObject p in players) {
+            _playerMng[i] = p.GetComponent<PlayerManager>();
+            _playerMng[i].SetScore(startScore);
+            i += 1;
+        }
+    }
+}
